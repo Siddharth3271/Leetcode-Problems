bc666df9e616247a43efd91806769ab242e56c75
Siddharth Shukla <122201651+Siddharth3271@users.noreply.github.com>
2024-02-29 23:33:41 +0530

    Create bulb_switcher.cpp

---
+++ bulb_switcher.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        //check for no. of perfect square
+        //one extra pair left for any perfect square
+        return sqrt(n);  //O(1) time and space
+    }
+};
