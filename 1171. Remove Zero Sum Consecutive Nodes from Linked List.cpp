ff4fabb8ee56571d266e2ba9d5511e9619231c74
Siddharth Shukla <122201651+Siddharth3271@users.noreply.github.com>
2024-03-12 23:46:03 +0530

    Create 1171. Remove Zero Sum Consecutive Nodes from Linked List.cpp

---
+++ 1171. Remove Zero Sum Consecutive Nodes from Linked List.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    //T.C.=O(n)
+    //S.C.=O(n)
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        int prefixSum=0;
+        unordered_map<int,ListNode*>mp;
+        ListNode* dummyNode=new ListNode(0);
+        dummyNode->next=head;
+        mp[0]=dummyNode;
+        while(head!=NULL){
+            prefixSum+=head->val;
+            if(mp.find(prefixSum)!=mp.end()){
+                //start deletion
+                ListNode* start=mp[prefixSum]; //last time jisko dekha tha
+                ListNode* temp=start;
+                int pSum=prefixSum;
+                while(temp!=head){
+                    temp=temp->next;
+                    pSum+=temp->val;
+                    if(temp!=head)
+                        mp.erase(pSum);
+                }
+                start->next=head->next;
+            }
+            else{
+                mp[prefixSum]=head;
+            }
+            head=head->next;
+        }
+        return dummyNode->next;
+    }
+};
